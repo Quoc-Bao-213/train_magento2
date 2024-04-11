837b1ad626d43fa11e5f45f6803941ac79b4900e
Bao ta <bao.ta@s3corp.com.vn>
2024-04-11 16:53:15 +0700

    Feat(setup): Init base source

---
+++ app/etc/env.php
@@ -0,0 +1,89 @@
+<?php
+return [
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'D7nzyhd6ghxGKPyBjYTPJ0ENsScWdHua'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'ca3_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'ca3_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base64ho/1OruKwDU+VmCmet4ZqvJVvm0txxJFQuLUNsv/Wbs='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost:3307',
+                'dbname' => 'magento',
+                'username' => 'root',
+                'password' => '',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'graphql_query_resolver_result' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'baotatrummagento.com'
+    ],
+    'install' => [
+        'date' => 'Thu, 11 Apr 2024 04:18:22 +0000'
+    ]
+];
